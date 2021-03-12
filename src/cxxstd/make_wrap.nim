9633949f8d279a2ec6150108dd66334f6350197a
haxscramper <haxscramper@gmail.com>
2021-03-12 23:02:46 +0300

    [SKIP]

---
+++ src/cxxstd/make_wrap.nim
@@ -0,0 +1,143 @@
+import hcparse/[wrap_common]
+import std/[tables]
+
+let multifileTable* = toTable({
+  "string" : @[
+    "basic_string.tcc",
+    "basic_string.h",
+    "stringfwd.h"
+  ],
+  "unordered_set" : @[
+    "unordered_set.h"
+  ],
+  "iterator": @[
+    "stl_iterator_base_types.h",
+    "stl_iterator_base_funcs.h",
+    "stl_iterator.h",
+    "stream_iterator.h",
+    "streambuf_iterator.h",
+    "range_access.h",
+  ]
+})
+
+let includeRemaps* = toTable({
+  "bits/basic_string.h" : "string",
+  "bits/stringfwd.h" : "string",
+  "bits/basic_string.tcc" : "string",
+  "bits/unordered_set.h" : "unordered_set",
+  "bits/stl_vector.h" : "vector"
+})
+
+let
+  basedir = AbsDir("/usr/include/c++/10.2.0")
+  gnuDir = RelDir("x86_64-pc-linux-gnu")
+
+let parseConf* = baseCppParseConfig.withIt do:
+  it.includepaths.add @[baseDir, baseDir / gnuDir]
+
+  it.globalFlags = @["-xc++"]
+
+
+
+let wrapConf* = baseCppWrapConf.withDeepIt do:
+  it.parseConf = parseConf
+  it.baseDir = baseDir
+  it.collapsibleNamespaces = @["cxx11", "__gnu_cxx"]
+
+  it.makeHeader = (
+    proc(cursor: CXCursor, conf: WrapConfig): NimHeaderSpec {.closure.} =
+      let path = cursor.asGlobalInclude(conf)
+      initHeaderSpec includeRemaps.getOrDefault(path, path)
+  )
+
+  it.ignoreCursor = (
+    proc(cursor: CXCursor, conf: WrapConfig): bool =
+      if $cursor == "__gnu_cxx":
+        false
+
+      else:
+        baseCppWrapConf.ignoreCursor(cursor, conf)
+  )
+
+  it.getImport = (
+    proc(dep: AbsFile, conf: WrapConfig): seq[string] {.closure.} =
+      if dep.startsWith("/usr/include/c++"):
+        var dep: RelFile = dep.withoutPrefix(baseDir)
+        if $dep in includeRemaps:
+          dep = RelFile(includeRemaps[$dep])
+
+        let (dir, name, ext) = dep.splitFile()
+        @["cxxstd", name.fixFileName()]
+
+      else:
+        baseCppWrapConf.getImport(dep, conf)
+  )
+
+  it.isInLibrary = (
+    proc(dep: AbsFile): bool {.closure.} =
+      dep.startsWith("/usr/include/c++")
+  )
+
+  it.depResolver = (
+    proc(
+      cursor, referencedBy: CXCursor
+    ): DepResolutionKind {.closure.} =
+      if cursor.isFromMainFile():
+        result = drkWrapDirectly
+
+      else:
+        if cursor.getSpellingLocation().getSome(loc) and
+          loc.file.splitFile2().file in ["????"]:
+            return drkIgnoreIfUsed
+
+        elif referencedBy.cxKind() == ckTranslationUnit:
+          let tuFile: AbsFile = referencedBy.getTuFile()
+          let cfile = cursor.getSpellingLocation()
+
+          if cfile.isSome():
+            let cursorFile = cfile.get().file.splitFile2().file
+            let tufile: string = tuFile.splitFile2().file
+
+            if tuFile in multifileTable and
+               cursorFile in multifileTable[tuFile]:
+              return drkWrapDirectly
+
+            else:
+              return drkImportUses
+
+        else:
+          return drkImportUses
+
+  )
+
+
+when isMainModule:
+  startColorLogger(showfile = true)
+
+  const cxxStdHeaders = [
+    "string", "cppconfig", "iterator"
+  ]
+
+  var files = collect(newSeq):
+    for file in walkDir(AbsDir(basedir), RelFile, recurse = false):
+      if file.ext() == "" and file.name() in cxxStdHeaders:
+        file
+
+  # files &= 
+
+  wrapWithConfig(
+    baseDir / gnuDir / "bits" /. "c++config.h",
+    RelFile("cppconfig.nim"),
+    wrapConf, parseConf
+  )
+
+  for file in files:
+    let res = file.withExt("nim")
+    debug baseDir / file, "->", res
+    wrapWithConfig(baseDir / file, res, wrapConf, parseConf)
+
+  for file in files:
+    info "Checking", file
+    execShell shCmd(nim, check, warnings=off, errorMax=2, $file.withExt("nim"))
+
+  info "Done"

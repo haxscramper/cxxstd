451dbd1d6750f2653c0b3a7c9a983386553a8072
haxscramper <haxscramper@gmail.com>
2021-06-28 21:47:48 +0300

    [CHECKPOINT]

--- src/cxxstd/make_wrap.nim
+++ src/cxxstd/make_wrap.nim
@@ -1,5 +1,6 @@
 import hcparse/[wrap_common]
-import std/[tables]
+import std/[tables, strformat]
+import hmisc/other/hlogger
 
 let multifileTable* = toTable({
   "string" : @[
@@ -17,7 +18,10 @@ let multifileTable* = toTable({
     "stream_iterator.h",
     "streambuf_iterator.h",
     "range_access.h",
-  ]
+  ] # ,
+  # "istream": @[
+
+  # ]
 })
 
 let includeRemaps* = toTable({
@@ -29,10 +33,10 @@ let includeRemaps* = toTable({
 })
 
 let
-  basedir = AbsDir("/usr/include/c++/10.2.0")
+  basedir = AbsDir(&"/usr/include/c++/").listDirs()[0]
   gnuDir = RelDir("x86_64-pc-linux-gnu")
 
-let parseConf* = baseCppParseConfig.withIt do:
+let parseConf* = baseCppParseConf.withIt do:
   it.includepaths.add @[baseDir, baseDir / gnuDir]
 
   it.globalFlags = @["-xc++"]
@@ -42,16 +46,17 @@ let parseConf* = baseCppParseConfig.withIt do:
 let wrapConf* = baseCppWrapConf.withDeepIt do:
   it.parseConf = parseConf
   it.baseDir = baseDir
+  it.nimOutDir = cwd()
   it.collapsibleNamespaces = @["cxx11", "__gnu_cxx"]
 
   it.makeHeader = (
-    proc(cursor: CXCursor, conf: WrapConfig): NimHeaderSpec {.closure.} =
+    proc(cursor: CXCursor, conf: WrapConf): NimHeaderSpec {.closure.} =
       let path = cursor.asGlobalInclude(conf)
       initHeaderSpec includeRemaps.getOrDefault(path, path)
   )
 
   it.ignoreCursor = (
-    proc(cursor: CXCursor, conf: WrapConfig): bool =
+    proc(cursor: CXCursor, conf: WrapConf): bool =
       if $cursor == "__gnu_cxx":
         false
 
@@ -60,7 +65,7 @@ let wrapConf* = baseCppWrapConf.withDeepIt do:
   )
 
   it.getImport = (
-    proc(dep: AbsFile, conf: WrapConfig, isExternalImport: bool):
+    proc(dep: AbsFile, conf: WrapConf, isExternalImport: bool):
       NimImportSpec {.closure.} =
 
       assert conf.isInLibrary(dep, conf), $dep
@@ -72,12 +77,15 @@ let wrapConf* = baseCppWrapConf.withDeepIt do:
       result = asImportFromDir(
         absoluteDep, conf, conf.baseDir, isExternalImport)
 
-      info result
-      result.importPath = @["cxxstd"] & result.importPath
+      if isExternalImport:
+        result.importPath = @["cxxstd"] & result.importPath
+
+      # if not isExternalImport:
+      #   update
   )
 
   it.isInLibrary = (
-    proc(dep: AbsFile, conf: WrapConfig): bool {.closure.} =
+    proc(dep: AbsFile, conf: WrapConf): bool {.closure.} =
       dep.startsWith(conf.baseDir)
   )
 
@@ -119,29 +127,38 @@ when isMainModule:
   startHax()
 
   const cxxStdHeaders = [
-    "string", "cppconfig", "iterator"
+    "string", "cppconfig", "iterator", "istream"
   ]
 
   var files = collect(newSeq):
-    for file in walkDir(AbsDir(basedir), RelFile, recurse = false):
+    for file in walkDir(AbsDir(basedir), AbsFile, recurse = false):
       if file.ext() == "" and file.name() in cxxStdHeaders:
         file
 
+
+  var logger = newTermLogger()
+
+  try:
+    wrapAllFiles(files, wrapConf, parseConf)
+
+  except Exception as e:
+    logger.logStackTrace(e)
   # files &=
 
-  wrapWithConfig(
-    baseDir / gnuDir / "bits" /. "c++config.h",
-    RelFile("cppconfig.nim"),
-    wrapConf, parseConf
-  )
+  # wrapWithConf(
+  #   baseDir / gnuDir / "bits" /. "c++config.h",
+  #   RelFile("cppconfig.nim"),
+  #   wrapConf, parseConf
+  # )
 
-  for file in files:
-    let res = cwd() / file.withExt("nim")
-    debug baseDir / file, "->", res
-    wrapWithConfig(baseDir / file, res, wrapConf, parseConf)
+  # for file in files:
+  #   let res = cwd() / file.withExt("nim")
+  #   debug baseDir / file, "->", res
+  #   wrapWithConfig(baseDir / file, res, wrapConf, parseConf)
 
-  for file in files:
-    info "Checking", file
-    execShell shCmd(nim, check, warnings=off, errorMax=2, $file.withExt("nim"))
+  # for file in files:
+  #   info "Checking", file
+  #   execShell shCmd(nim, check, warnings=off, errorMax=2, $file.withExt("nim"))
 
-  info "Done wrapping C++ standard library"
+  # info "Done wrapping C++ standard library"
+  echo "done"
